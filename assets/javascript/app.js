4dcaf2d2a35bf931846fa410c98213e422a7ce6d
Kurtis Moffett <moffkr91@gmail.com>
2018-03-14 20:35:13 -0700

    firebase key added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,23 @@
 $( document ).ready(function () {
+var config = {
+    apiKey: "AIzaSyDy000v5zr6p96yAq-iupooKjesEPRBwtM",
+    authDomain: "bloody-unicorns.firebaseapp.com",
+    databaseURL: "https://bloody-unicorns.firebaseio.com",
+    projectId: "bloody-unicorns",
+    storageBucket: "bloody-unicorns.appspot.com",
+    messagingSenderId: "43519404374"
+    };
+    firebase.initializeApp(config);
 
-
-
-
-
+let database = firebase.database()
 
     
-    
-    
+
+
+
+
+
+
+
+
 })
