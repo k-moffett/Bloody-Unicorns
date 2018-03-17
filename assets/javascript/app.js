7eec8fa00564e59dee6d7615c983a72a78badb7d
Kurtis Moffett <moffkr91@gmail.com>
2018-03-17 14:14:34 -0700

    quicksave

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,22 @@
 $( document ).ready(function () {
 
 
-let queryURL = ""
+let apiURL = "https://accounts.spotify.com/authorize?client_id=fa3f02378e7049139d059733f8e59a19&response_type=code&redirect_uri=http://localhost:8888/callback"
+
+$("#submit").on("click", function(){
+event.preventDefault()
+var corsProxy = "https://cors-anywhere.herokuapp.com/";
+var apiUrl = "https://your-api-url/";
+var url = corsProxy + apiUrl;
 
 $.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-  
+    url: url,
+    type: 'GET',
+}).then(function(data) { 
+    console.log(data);
+});
 });
 
 
-
-});
+//////
+})
